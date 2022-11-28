505c3809aa48410cb462cd6cc1690e11f90d7dd7
RutcunOficial <rutcunoficial@gmail.com>
2022-11-28 10:29:08 -0500

    Se modifico Inicio y se agrego funcionalidad

--- PRutCun/Startup.cs
+++ PRutCun/Startup.cs
@@ -53,7 +53,7 @@ namespace PRutCun
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Login}/{action=Index}/{id?}");
+                    pattern: "{controller=Inicio}/{action=Index}/{id?}");
             });
         }
     }
