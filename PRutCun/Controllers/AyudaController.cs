b5c0f17514b494b42bda2db5401efbbf82aa97b2
RutcunOficial <rutcunoficial@gmail.com>
2022-11-29 11:06:48 -0500

    Christian y Bryan Pagina de ayuda

---
+++ PRutCun/Controllers/AyudaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRutCun.Controllers
+{
+    public class AyudaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
